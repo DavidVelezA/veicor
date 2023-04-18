88d3f7514d78075f6bc24ad9798048ee775617cb
David Velez <evelez@eluniverso.com>
2023-04-18 10:47:45 -0500

    cambios

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-'use strict'
+"use strict";
 // conectar a la base de datos MongoDB
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 // exportar configuracion de express
-var app = require('./app');
+var app = require("./app");
 var port = process.env.PORT || 3999;
 
-mongoose.set('useFindAndModify', false);
-mongoose.set('useUnifiedTopology', true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useUnifiedTopology", true);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://facci:facci@cluster0-kidus.mongodb.net/db-veicor', { useNewUrlParser: true, useCreateIndex: true })
-// mongoose.connect('mongodb://localhost:27017/api_rest_node', { useNewUrlParser: true })
-    .then(() => {
+mongoose
+  .connect("mongodb+srv://veicor:veicor@cluster0.isugy7m.mongodb.net/veicor", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  // mongoose.connect('mongodb://localhost:27017/api_rest_node', { useNewUrlParser: true })
+  .then(() => {
+    console.log("conexion a MongoDB correcta");
 
-        console.log('conexion a MongoDB correcta');
-
-        // crear servidor
-        app.listen(port, () => {
-            console.log("El servidor http://localhost:3999 esta funcionando");
-        });
-
-    })
-    .catch(error => console.log(error));
+    // crear servidor
+    app.listen(port, () => {
+      console.log("El servidor http://localhost:3999 esta funcionando");
+    });
+  })
+  .catch((error) => console.log(error));
